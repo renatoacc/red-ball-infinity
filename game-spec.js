1bd524473aa9b6d37b7a9bdeeb42331c7b68e5e4
Renato Costa <renatoaccosta@gmail.com>
2022-03-04 10:28:02 +0000

    music update

--- game-spec.js
+++ game-spec.js
@@ -49,7 +49,7 @@ const painel = document.querySelector(".painel");
 window.onload = () => {
   btnStart.onclick = () => {
     startGame();
-    mainSound.play();
+    mainSound.loop();
   };
   btnRestart.onclick = () => {
     //isGameOver = false;
